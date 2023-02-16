427dba5ef788ad53f20432c5425dd3df3d904bd0
ArthurD3nt <francescomazzucco01@gmail.com>
2023-02-16 14:16:29 +0100

    Initial build skeleton created

---
+++ client/src/main/java/MainClient.java
@@ -0,0 +1,9 @@
+public class MainClient {
+
+  /**
+   * Main method
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello world I'm the client");
+  }
+}
---
+++ server/src/main/java/MainServer.java
@@ -0,0 +1,9 @@
+public class MainServer {
+
+  /**
+   * Main method
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello world I'm the server");
+  }
+}
