48c31d159f952d2c9a29bef66b3abdca0b471cbe
ArthurD3nt <francescomazzucco01@gmail.com>
2023-02-16 14:28:43 +0100

    Finished setting up gradle build system

---
+++ client/src/tests/java/MainClientTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class MainClientTest {
+
+  @Test
+  public void verifyNoExceptionThrown() {
+    GradleTutorial.main(new String[] {});
+  }
+}
---
+++ server/src/tests/java/MainServerTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class MainServerTest {
+
+  @Test
+  public void verifyNoExceptionThrown() {
+    GradleTutorial.main(new String[] {});
+  }
+}
